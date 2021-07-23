e3edc460bf637fae7a585aafa83d28bc026983b0
Daniel Shoemaker <danielshoe@charter.net>
2021-07-23 15:57:32 -0700

    updates javascript

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 console.log("Javascript is working");
 
 function loginBtnChange() {
-    document.querySelector("#loginBtn").innerHTML = "Logout";
+    var temp = document.querySelector("#loginBtn").innerHTML;
+
+    if (temp == "Login") {
+        document.querySelector("#loginBtn").innerHTML = "Logout";
+    } else {
+        document.querySelector("#loginBtn").innerHTML = "Login";
+    }
 }
 
-function removeAddDefBtn() {
-    document.querySelector("")
+function hide(element) {
+    element.remove();
+}
+
+function topLikes(element) {
+    alert("Ninja was liked");
 }
