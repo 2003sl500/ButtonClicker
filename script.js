f3b22d330dab86d8a64447affd72e0b298a5c9b6
Daniel Shoemaker <danielshoe@charter.net>
2021-07-23 13:15:52 -0700

    Updated htlm  css  js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Javascript is working");
+
+function loginBtnChange() {
+    document.querySelector("#loginBtn").innerHTML = "Logout";
+}
+
+function removeAddDefBtn() {
+    document.querySelector("")
+}
